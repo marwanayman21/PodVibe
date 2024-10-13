533b375c8c78f7d07890f3548d556ca1baa58a86
seifalrahman <130805731+seifalrahman@users.noreply.github.com>
2024-10-13 23:20:16 +0300

    Update Login.jsx

--- Podvibe client/src/components/auth/Login.jsx
+++ Podvibe client/src/components/auth/Login.jsx
@@ -1,42 +1,102 @@
-// src/components/Login.jsx
-import React from 'react';
-import { FaGooglePlusG, FaFacebookF, FaTwitter, FaMicrosoft } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-    return (
-        <form className="flex flex-col p-6">
-            <h1 className="text-center text-2xl text-white mb-4">Sign In</h1>
-            <div className="flex justify-center mb-4">
-                <ul className="flex items-center gap-x-6">
-                    <li>
-                        <Link  className='w-10 h-10 bg-neutral-700/30 hover:bg-neutral-700/50 rounded-full flex items-center justify-center text-neutral-100 text-base ease-in-out duration-100' title="Sign in with Google">
-                            <FaGooglePlusG size={24} />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link  className='w-10 h-10 bg-neutral-700/30 hover:bg-neutral-700/50 rounded-full flex items-center justify-center text-neutral-100 text-base ease-in-out duration-100' title="Sign in with Facebook">
-                            <FaFacebookF size={24} />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link  className='w-10 h-10 bg-neutral-700/30 hover:bg-neutral-700/50 rounded-full flex items-center justify-center text-neutral-100 text-base ease-in-out duration-100' title="Sign in with Twitter">
-                            <FaTwitter size={24} />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link  className='w-10 h-10 bg-neutral-700/30 hover:bg-neutral-700/50 rounded-full flex items-center justify-center text-neutral-100 text-base ease-in-out duration-100' title="Sign in with Microsoft">
-                            <FaMicrosoft size={24} />
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <input type="email" placeholder="Email" className="mb-2 p-2 rounded" />
-            <input type="password" placeholder="Password" className="mb-2 p-2 rounded" />
-            <a href="#" className="text-purple-400 text-sm text-right mb-2">Forget Your Password?</a>
-            <button type="submit" className="bg-purple-800 p-2 rounded text-white">Sign In</button>
-        </form>
-    );
-};
+const url = "http://localhost:8080/api";
 
-export default Login;
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [token, setToken] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setDisabled(!email || !password);
+  }, [email, password]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const response = await axios.post(`${url}/auth/signin`, {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        // Assuming the response contains the user data
+        localStorage.setItem("auth", JSON.stringify(response.data));
+        setToken(response.data.data); //stores Token
+        navigate("/"); 
+      }
+    } catch (error) {
+      console.error("Error Logging In:", error);
+      setError("Invalid email or password."); // Display error message
+    }
+  };
+
+  return (
+    <div className="w-full px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-lg text-center">
+        <h1 className="text-2xl font-bold sm:text-3xl">Log In to PodVibe</h1>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto mb-0 mt-8 max-w-md space-y-4"
+      >
+        <div>
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+            placeholder="Enter Email"
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+            placeholder="Enter Password"
+            required
+          />
+        </div>
+
+        {error && (
+          <div
+            className="p-4 my-4 text-sm text-red-800 rounded-lg bg-red-50"
+            role="alert"
+          >
+            {error}
+          </div>
+        )}
+
+        <div className="flex items-center justify-between">
+          <button
+            disabled={disabled}
+            type="submit"
+            className="disabled:opacity-50 disabled:cursor-not-allowed inline-block rounded-lg bg-green-800 px-5 py-3 text-sm font-medium text-white"
+          >
+            Log In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
